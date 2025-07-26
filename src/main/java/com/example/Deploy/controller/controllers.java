0322786a46eeb4e3dd0a457963caae9172650e1d
DashB <dash.bachute@gmail.com>
2025-07-26 23:36:20 +0530

    init commit

---
+++ src/main/java/com/example/Deploy/controller/controllers.java
@@ -0,0 +1,17 @@
+package com.example.Deploy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class controllers {
+	
+	
+	@GetMapping("/check")
+	public String check() {
+		return "Hello Dash app is started";
+	}
+
+}
